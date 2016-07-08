2f5d506b2f95b5ea11e7b9edbfbdb9f3d841a7e6
ajrominger <ajrominger@gmail.com>
2016-07-07 19:25:51 -0700

    checked all functions (make.mm, sim.lm, run.1sim) and they're working

--- arsenic_power_analysis/arsenic_power_anal.R
+++ arsenic_power_analysis/arsenic_power_anal.R
@@ -28,21 +28,23 @@ make.mm <- function(nfern, nplot) {
 ## function to generate data using matrix multiplication,
 ## run linear model on that simulated data and extract p-values
 ## function arguments are:
-## shared arguments with make.mm, as `sim.lm' calls `make.mm'
+## mm = the model matrix as returned by `make.mm'
 ## a.t = time effect (length 1, equal to difference between the two times)
 ## a.trt = treatment effect (length 7, each value is the difference between that treatment and the control)
 ## a.As = main effect of As in soil on As in ferns
 ## a.As.trt = interaction of treatments with As relationship (length 7)
 ## beta.sd = standard deviation of the random plot effect
 ## epsilon.sd = standard deviation of the residuals
+## nfern = number of ferns per plot
+## nplot = number of plots
 
-sim.lm <- function(a.t, a.trt, a.As, a.As.trt, beta.sd, epsilon.sd, nfern, nplot) {
+sim.lm <- function(mm, a.t, a.trt, a.As, a.As.trt, beta.sd, epsilon.sd, nfern, nplot) {
     n <- 2*nfern*nplot
     
     ## the model matrix, remember that column 1 is the intercept (i.e. all 1's)
     mm <- make.mm(nfern, nplot)
     
-    ## fill in x place-holders with simulated x values
+    ## fill in x place-holders in model matrix with simulated x values
     x <- rnorm(n)
     mm[, 10:17] <- mm[, 10:17] * x
     
@@ -60,8 +62,6 @@ sim.lm <- function(a.t, a.trt, a.As, a.As.trt, beta.sd, epsilon.sd, nfern, nplot
     treat <- as.factor(mm[, 3:9] %*% 1:7 + 1)
     pltID <- as.factor(rep(rep(1:nplot, each=nfern), 2))
     
-    browser()
-    
     ## fit models
     mod.full <- lmer(y ~ time.step + treat*x + (1|pltID), REML=FALSE)
     mod.null <- lmer(y ~ time.step + x + (1|pltID), REML=FALSE)
@@ -70,4 +70,18 @@ sim.lm <- function(a.t, a.trt, a.As, a.As.trt, beta.sd, epsilon.sd, nfern, nplot
     return(anova(mod.null, mod.full)[['Pr(>Chisq)']][2])
 }
 
-bla <- sim.lm(a.t=1, a.trt=1:7, a.As=0.5, a.As.trt=1:7, beta.sd=0, epsilon.sd = 0.5, nfern=3, nplot=100*3)
+
+## function to run the simulation for 1 set of parameters
+run.1sim <- function(nsim, a.t, a.trt, a.As, a.As.trt, beta.sd, epsilon.sd, nfern, nplot) {
+    mm <- make.mm(nfern, nplot)
+    
+    ## use replicate to return many simulations for these parameters
+    replicate(nsim, sim.lm(mm, a.t, a.trt, a.As, a.As.trt, beta.sd, epsilon.sd, nfern, nplot))
+}
+
+## example use (goal is to put this in a function that will iterate through all parameter combinations)
+## here's the case where there is *no* treatment effect (95% of pvals should be > 0.05 if nsim is large)
+run.1sim(nsim=10, a.t=1, a.trt=rep(0, 7), a.As=0.5, a.As.trt=rep(0, 7), beta.sd=0.5, epsilon.sd=0.1, nfern=3, nplot=16*3)
+
+## here's the case where there *is* a treatment effect (most pvals should be <= 0.05 if nsim is large)
+run.1sim(nsim=10, a.t=1, a.trt=1:7, a.As=0.5, a.As.trt=1:7, beta.sd=0.5, epsilon.sd=0.1, nfern=3, nplot=16*3)
