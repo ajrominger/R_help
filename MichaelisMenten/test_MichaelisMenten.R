bdfb5fb1f15168c94f059fd4166d456a0b0c6076
ajrominger <ajrominger@gmail.com>
2016-11-02 21:07:22 -0700

    removed plotting and added p-value tests

--- MichaelisMenten/test_MichaelisMenten.R
+++ MichaelisMenten/test_MichaelisMenten.R
@@ -11,14 +11,16 @@ x <- datatop[datatop$Site == 'A' & datatop$SRR > 0, ]
 
 mmsol(x$SRR, x$Avg_sulfate_conc_in_FTR)
 
-vOverS <- x$SRR / x$Avg_sulfate_conc_in_FTR
-SoverV <- x$Avg_sulfate_conc_in_FTR / x$SRR
-v <- x$SRR
-S <- x$Avg_sulfate_conc_in_FTR
-
-plot(vOverS, v, col = as.factor(x$Sulfate_txmt))
-plot(v, vOverS, col = as.factor(x$Sulfate_txmt))
-plot(S, SoverV, col = as.factor(x$Sulfate_txmt))
-plot(S, v)
-
 mmboot(x$SRR, x$Avg_sulfate_conc_in_FTR)
+
+mmttest(datatop$SRR[datatop$Site == 'B' & datatop$SRR > 0], 
+        datatop$Avg_sulfate_conc_in_FTR[datatop$Site == 'B' & datatop$SRR > 0],
+        datatop$SRR[datatop$Site == 'C' & datatop$SRR > 0],
+        datatop$Avg_sulfate_conc_in_FTR[datatop$Site == 'C' & datatop$SRR > 0],
+        n = 5000)
+
+mmbootP(datatop$SRR[datatop$Site == 'A' & datatop$SRR > 0], 
+        datatop$Avg_sulfate_conc_in_FTR[datatop$Site == 'A' & datatop$SRR > 0],
+        datatop$SRR[datatop$Site == 'B' & datatop$SRR > 0],
+        datatop$Avg_sulfate_conc_in_FTR[datatop$Site == 'B' & datatop$SRR > 0], 
+        n = 5000)
